0e892dd8c7da0592ade26fffcb3180d54ffbcc81
Magikinashep <69910665+Magikinashep@users.noreply.github.com>
2022-08-28 10:43:50 +0800

    Create weapons.java

---
+++ exercise_1/weapons.java
@@ -0,0 +1,12 @@
+public class weapons {
+    String name;
+    int damage;
+    double weight;
+    String rarity;
+    String description;
+
+        //Description method
+        public void weaponDescription() {
+            System.out.println(description);
+        }
+}
